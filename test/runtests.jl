b1964cc578496cf7aa4d498aa429c42e4f5aaf93
vinicius viena santana <vinicius.viena1@gmail.com>
2024-08-04 22:51:53 +0200

    Including documentation pages with documenter.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,7 @@ using Test
 #we test that definitions of loading and sp_res are consistent.
 
 function test_sp_res_loading(model, prange, T)
+    
     for p in prange
         n = loading(model,p,T)
         n₀ = loading_ad(model,p,T)
@@ -18,12 +19,13 @@ end
 @testset "AdsorbedSolutionTheory.jl" begin
     #Langmuir testing
     cL_test = Langmuir(1.727, 16.71e-10, -16152.50)
-    
+
         # Loading
         test_sp_res_loading(cL_test, [1e5, 2e5, 3e5], 298.)
 
         # Isosteric heat
         V = Rgas(cL_test)*298.0/101325.0
         ΔH = isosteric_heat(cL_test, V, 101325.0, 298.) ≈ -cL_test.E
+
 end
 
