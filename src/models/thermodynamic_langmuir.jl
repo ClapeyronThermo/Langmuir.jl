a84c47321dcf03330d73d1f8657d6fccd962a257
vinicius viena santana <vinicius.viena1@gmail.com>
2024-10-30 22:56:49 +0100

    added henry and isosteric heat for thermodynamic langmuir

--- src/models/thermodynamic_langmuir.jl
+++ src/models/thermodynamic_langmuir.jl
@@ -35,6 +35,46 @@ function loading_x0(model::ThermodynamicLangmuir, p, T)
     return loading(guess_model, p, T)
 end
 
+function isotherm_res(model::ThermodynamicLangmuir, q, p, T)
+    _1 = one(eltype(model))
+    M = model.M
+    K₀ = model.K₀
+    E = model.E
+    K = K₀*exp(-E/(Rgas(model)*T))
+    θᵢ = q/M
+    γᵢ, γᵩ = activity_coefficient(model, T, [θᵢ, _1 - θᵢ])
+    return q - M * K *p / (γᵢ/γᵩ + K*p) 
+end
+
+function henry_coefficient(model::ThermodynamicLangmuir, T)
+
+    _0 = zero(eltype(T))
+
+    q_0 = loading(model, _0, T)
+
+    f(∂q, ∂p) = isotherm_res(model, ∂q, ∂p, T)
+
+    _f,_df = fgradf2(f, q_0, _0)
+
+    ∂f0_∂q, ∂f0_∂P = _df
+    
+    return -∂f0_∂P/∂f0_∂q
+end
+
+function isosteric_heat(model::ThermodynamicLangmuir, p, T; Vᵃ = zero(eltype(p)), Vᵍ = Rgas(model)*T/p)
+
+    q = loading(model, p, T)
+
+    f(∂p, ∂T) = isotherm_res(model, q, ∂p, ∂T)
+
+    _f,_df = fgradf2(f, p, T)
+
+    ∂n_∂p, ∂n_∂T = _df
+
+    return T*(Vᵍ - Vᵃ)*∂n_∂T/∂n_∂p
+
+end
+
 function loading_impl(model::ThermodynamicLangmuir, p, T)
 
     _1 = one(eltype(p))
